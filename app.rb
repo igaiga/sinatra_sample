0781f9c6adfef997a6d1fa84e7e1e2ea5006a14a
Kuniaki IGARASHI <igaiga@gmail.com>
2014-09-15 23:29:16 +0000

    Add nitrous.io setting codes

--- app.rb
+++ app.rb
@@ -2,6 +2,12 @@
 # We can specify port number with -p XXXX .
 require 'sinatra'
 
+# For Nitrous.io
+configure :development do
+  set :bind, '0.0.0.0'
+  set :port, 3000
+end
+
 get '/' do
   "Hello world!"
 end
